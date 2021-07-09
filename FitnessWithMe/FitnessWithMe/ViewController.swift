b1555324a33c31505158935fda632fca8b81412b
maryzhao <tt@Marys-MacBook-Pro.local>
2021-07-08 20:37:42 -0400

    added textbox and text view

--- FitnessWithMe/FitnessWithMe/ViewController.swift
+++ FitnessWithMe/FitnessWithMe/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var workout1: UITextField!
+    @IBOutlet weak var displayWorkout: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func enterWorkout1(_ sender: UIButton) {
+        let mtext = workout1.text
+        displayWorkout.text = mtext
+    }
 
+    @IBAction func appendWorkout(_ sender: UIButton) {
+        let mtext = workout1.text
+        displayWorkout.text += "\n"
+        displayWorkout.text +=  mtext ?? ""
+    }
+    
 }
 
